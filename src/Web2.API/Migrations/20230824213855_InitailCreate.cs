2470bfc9b10c89617ab72a951f47a8e8c092dfb1
Jason1 <funhoster@hotmail.ca>
2023-08-24 17:39:22 -0400

    add migration

---
+++ src/Web2.API/Migrations/20230824213855_InitailCreate.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Web2.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitailCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Evenements",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Titre = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    DateDebut = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Organisateur = table.Column<string>(type: "text", nullable: false),
+                    DateFin = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Adresse = table.Column<string>(type: "text", nullable: false),
+                    Prix = table.Column<double>(type: "double precision", nullable: true),
+                    VilleID = table.Column<int>(type: "integer", nullable: true),
+                    CategoryIDs = table.Column<List<int>>(type: "integer[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evenements", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participations",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Nom = table.Column<string>(type: "text", nullable: false),
+                    Prenom = table.Column<string>(type: "text", nullable: false),
+                    NombrePlace = table.Column<int>(type: "integer", nullable: false),
+                    EvenementId = table.Column<int>(type: "integer", nullable: true),
+                    IsValid = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participations", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Villes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Region = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Villes", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Evenements");
+
+            migrationBuilder.DropTable(
+                name: "Participations");
+
+            migrationBuilder.DropTable(
+                name: "Villes");
+        }
+    }
+}
