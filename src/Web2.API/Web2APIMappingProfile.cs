ec52c4b9d5f5b00cc8bf4b4192a95a845ba841ce
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-08-27 12:19:18 -0400

    Ajout de la classe MappingProfile

---
+++ src/Web2.API/Web2APIMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Web2.API.Data.Models;
+using Web2.API.DTO;
+
+namespace Web2.API
+{
+    public class Web2APIMappingProfile : Profile
+    {
+        public Web2APIMappingProfile()
+        {
+            CreateMap<Categorie, CategorieDTO>();
+            CreateMap<Ville, VilleDTO>();
+            CreateMap<Evenement, EvenementDTO>();
+            CreateMap<Participation, ParticipationDTO>();
+
+            //CreateMap<Student, StudentDTO>()
+            //    .ForMember(dest => dest.EnrollmentIds, opt => opt.MapFrom(src => src.Enrollments.Select(e => e.ID).ToList()));
+            
+            ////.ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Course.Title))
+            ////.ForMember(dest => dest.CourseID, opt => opt.MapFrom(src => src.Course.CourseID))
+            ////.ForMember(dest => dest.Credits, opt => opt.MapFrom(src => src.Course.Credits));
+        }
+    }
+}
