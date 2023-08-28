d68e73b6046f9d9fd6f63e5482e193bc22ac4a63
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-08-27 23:29:05 -0400

    Ajout du début de la question 11

---
+++ src/Web2.API/Services/IStatistiquesService.cs
@@ -0,0 +1,10 @@
+﻿using Web2.API.DTO;
+
+namespace Web2.API.Services
+{
+    public interface IStatistiquesService
+    {
+        List<VilleDTO> GetVillesPopulaires();
+        List<EvenementDTO> GetEvenementsRentables();
+    }
+}
